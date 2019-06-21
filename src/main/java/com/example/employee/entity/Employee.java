1792cf5409cb019f2db2080b9a3908c5f611cbe6
humplee <littleted1221@163.com>
2019-06-21 11:14:42 +0800

    entity done

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,53 @@
 package com.example.employee.entity;
 
 public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int companyId;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public Employee(int id, String name, int age, String gender, int companyId, int salary){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.companyId=companyId;
+        this.salary=salary;
+    }
+
 
 }
+//    CREATE TABLE Employee(
+//        id INT PRIMARY KEY,
+//        name VARCHAR(255) NOT NULL,
+//    age INT NOT NULL,
+//    gender VARCHAR(10),
+//    companyId INT NOT NULL,
+//    salary INT NOT NULL
+//) engine=InnoDB DEFAULT CHARSET = utf8;
